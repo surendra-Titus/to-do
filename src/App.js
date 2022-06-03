b3450f59e9569fdb9e1c0d772d6c3aaabe7fcd0b
Naga Surendra <naga.surendra@mariapps.com>
2022-06-03 19:33:50 +0530

    installed React Modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import ToDoClass from "./Components/ToDoClass";
 import ToDo from "./Components/ToDo";
+import Modal from "react-modal/lib/components/Modal";
+
+Modal.setAppElement('#root')
 
 const App = () => {
   return (
