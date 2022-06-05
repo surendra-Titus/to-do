a3e481b1d8658ff25ec69971f167b1b2a99a0b58
Naga Surendra <naga.surendra@mariapps.com>
2022-06-05 22:48:29 +0530

    Added checkbox to the list for status of ToDo's.

--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -1,22 +1,63 @@
 import React from "react";
 
-const ToDoList = ({ item, deleteEventHandler, editEvnetHandler }) => {
+const ToDoList = ({
+  item,
+  todos,
+  setToDo,
+  todo,
+  setInputText,
+  setIsOpen,
+  isOpen,
+}) => {
+  const isCheckedHandler = () => {
+    setToDo(
+      todos.map((element) => {
+        if (element.id === item.id) {
+          return {
+            ...element,
+            isChecked: !element.isChecked,
+          };
+        }
+        return element;
+      })
+    );
+  };
+
+  const editEvnetHandler = (event) => {
+    setInputText(event.todo);
+    setToDo(todos.filter((item) => item.id !== event.id));
+  };
+
+  const deleteEventHandler = async (event) => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
       <ul className="list-group">
         <li className="list-group-item d-flex justify-content-between align-items-center">
-          {item}
+          <div>
+            <input
+              onChange={isCheckedHandler}
+              className="list-group-input"
+              type="checkbox"
+              value=""
+              id="flexCheckBox"
+            />
+            <span> </span>
+            {item.todo}
+          </div>
           <div className="float-end">
             <button
               className="btn btn-outline-primary me-md-2"
-              onClick={editEvnetHandler}
+              onClick={() => editEvnetHandler(todo)}
             >
               Edit
             </button>
             <span> </span>
             <button
               className="btn btn-outline-danger"
-              onClick={deleteEventHandler}
+              onClick={() => deleteEventHandler(todo.id)}
             >
               Delete
             </button>
